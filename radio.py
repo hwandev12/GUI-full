8738d517ca2e41576f353da21cae807a6b26213b
Husan <102412893+hwandev12@users.noreply.github.com>
2022-07-29 09:48:29 +0500

    update radio buttons

--- radio.py
+++ radio.py
@@ -8,14 +8,31 @@ root.iconbitmap("C:/Users/husan/Downloads/fresh.ico")
 r = IntVar()
 r.set("2")
 
+MODES = [
+    ("Ram", "Ram"),
+    ("Memory", "Memory"),
+    ("Camera", "Camera"),
+    ("Screen", "Screen")
+]
+
+phone = StringVar()
+phone.set("Ram")
+
+for text, mode in MODES:
+    Radiobutton(root, text=text, variable=phone, value=mode).pack(anchor=W)
+
+
 def clicked(value):
     myLabel = Label(root, text=value)
     myLabel.pack()
 
-Radiobutton(root, text="Option 1", variable=r, value=1, command=lambda: clicked(r.get())).pack()
-Radiobutton(root, text="Option 2", variable=r, value=2, command=lambda: clicked(r.get())).pack()
+# Radiobutton(root, text="Option 1", variable=r, value=1, command=lambda: clicked(r.get())).pack()
+# Radiobutton(root, text="Option 2", variable=r, value=2, command=lambda: clicked(r.get())).pack()
 
-myLabel = Label(root, text=r.get())
-myLabel.pack()
+# myLabel = Label(root, text=phone.get())
+# myLabel.pack()
+
+button = Button(root, text="Click me!", command=lambda: clicked(phone.get()))
+button.pack()
 
 root.mainloop()
